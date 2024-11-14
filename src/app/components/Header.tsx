324ab178785bc0140d9a9510d28c2571ef1b007c
fiza  shahbaz <155994265+Fizashahbaz@users.noreply.github.com>
2024-11-14 16:25:11 +0500

    first commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=" w-[1922px] h-[100px] bg-[#A29875]">
+ <h1 className="text-[32px]   text-[#FFFFFF] p-6">MANZZARI</h1>
+
+
+    </div>
+  )
+}
+
+export default Header
